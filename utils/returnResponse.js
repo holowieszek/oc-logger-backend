294d5d1193e853f752039218ff79efc3b42e12a7
Mateusz Holowieszko <mateusz.holowieszko@ttpsc.pl>
2019-10-22 16:45:38 +0200

    Added response headers to returnResponse helper

--- utils/returnResponse.js
+++ utils/returnResponse.js
@@ -1,6 +1,10 @@
 const returnResponse = (status, value) => {
   return {
     statusCode: status,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+    },
     body: JSON.stringify(value)
   }
 }
