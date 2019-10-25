2b236e3240a2c1c7c50dd967bb6a0f8bdca488d3
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-26 01:49:26 +0200

    Lambda for listing geocaches contains now is_found parameter

--- OpenCaching/GetGeoCache.js
+++ OpenCaching/GetGeoCache.js
@@ -1,22 +1,33 @@
 const rp = require('request-promise');
 const asyncWrapper = require('../utils/asyncWrapper');
 const returnResponse = require('../utils/returnResponse');
+const oauth = require('../OAuth/oauth');
 
 const invoke = async event => {
   console.log(event);
 
-  const { cacheCode } = JSON.parse(event.body);
+  const { cacheCode, oauthToken, oauthTokenSecret } = JSON.parse(event.body);
 
   const data = {
-    cache_code: cacheCode,
-    consumer_key: process.env.CONSUMER_KEY
+    url: 'https://opencaching.pl/okapi/services/caches/geocache',
+    method: 'GET',
+    data: {
+      cache_code: cacheCode,
+      oauth_consumer_key: process.env.CONSUMER_KEY,
+      fields: 'code|name|location|url|is_found'
+    }
+  }
+
+  const token = {
+    key: oauthToken,
+    secret: oauthTokenSecret
   }
 
   const { error, result } = await asyncWrapper(rp(
     {
-      url: 'https://opencaching.pl/okapi/services/caches/geocache',
-      method: 'GET',
-      qs: data,
+      url: data.url,
+      method: data.method,
+      qs: oauth.authorize(data, token),
       json: true
     }
   ));
